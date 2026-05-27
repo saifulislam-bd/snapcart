6dab349d5649999882924c12949ab63d0a0a774b
saifulislam-bd <saifulislam.dev22@gmail.com>
2026-05-27 22:11:09 +0600

    user model

---
+++ models/user.model.ts
@@ -0,0 +1,29 @@
+import mongoose, { Schema } from "mongoose";
+
+interface IUser {
+  _id: mongoose.Types.ObjectId;
+  name: string;
+  email: string;
+  password: string;
+  mobile: string;
+  role: "user" | "admin" | "deliveryBoy";
+}
+
+const userSchema = new Schema<IUser>(
+  {
+    name: { type: String, required: true },
+    email: { type: String, unique: true, required: true },
+    password: { type: String, required: true },
+    mobile: { type: String, required: true },
+    role: {
+      type: String,
+      required: true,
+      enum: ["user", "admin", "deliveryBoy"],
+      default: "user",
+    },
+  },
+  { timestamps: true },
+);
+
+const User = mongoose.models.User || mongoose.model("User", userSchema);
+export default User;
