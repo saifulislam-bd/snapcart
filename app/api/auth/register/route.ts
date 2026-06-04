d23b0b404a86da4d3b259f62aab99016703229d7
saifulislam-bd <saifulislam.dev22@gmail.com>
2026-06-04 23:22:55 +0600

    post route added

--- app/api/auth/register/route.ts
+++ app/api/auth/register/route.ts
@@ -1,7 +1,24 @@
+import User from "@/models/user.model";
 import { NextRequest } from "next/server";
 
 export async function POST(req: NextRequest) {
   try {
+    const body = await req.json();
+    if (!body.email || !body.password || !body.name || !body.mobile) {
+      return new Response(
+        JSON.stringify({ message: "All fields are required" }),
+        {
+          status: 400,
+          headers: { "Content-Type": "application/json" },
+        },
+      );
+    }
+
+    const user = await User.create(body);
+    return new Response(JSON.stringify({ user }), {
+      status: 200,
+      headers: { "Content-Type": "application/json" },
+    });
   } catch (error) {
     console.error(error.message);
   }
