730582c1a92cee8c96caae972f7b4032a102dab6
saifulislam-bd <saifulislam.dev22@gmail.com>
2026-06-10 20:25:04 +0600

    changes

--- app/page.tsx
+++ app/page.tsx
@@ -1,3 +1,7 @@
 export default function Home() {
-  return <div>Hello from Snapcart</div>;
+  return (
+    <main className="flex min-h-screen flex-col items-center justify-between p-24">
+      <h1 className="text-3xl font-bold underline">Hello Snapcart!</h1>
+    </main>
+  );
 }
