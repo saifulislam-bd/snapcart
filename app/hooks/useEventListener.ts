e18e27b0cf239a26c5ae665d4c7b416a823c8ac6
saifulislam-bd <saifulislam.dev22@gmail.com>
2026-06-24 23:32:20 +0600

    useEventListener custom hook

---
+++ app/hooks/useEventListener.ts
@@ -0,0 +1,21 @@
+import { useEffect, useRef } from "react";
+
+export default function useEventListener(
+  eventType: string,
+  callback: (e: Event) => void,
+  element: EventTarget | null = window,
+): void {
+  const callbackRef = useRef<(e: Event) => void>(callback);
+
+  useEffect(() => {
+    callbackRef.current = callback;
+  }, [callback]);
+
+  useEffect(() => {
+    if (element == null) return;
+    const handler = (e: Event) => callbackRef.current(e);
+    element.addEventListener(eventType, handler);
+
+    return () => element.removeEventListener(eventType, handler);
+  }, [eventType, element]);
+}
