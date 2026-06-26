a23ef8c8a96235e907709d18cfbb014aa5d5b74c
saifulislam-bd <saifulislam.dev22@gmail.com>
2026-06-26 23:26:53 +0600

    useStateWithValidation custom hook

---
+++ app/hooks/useStateWithValidation.ts
@@ -0,0 +1,25 @@
+import { useState, useCallback } from "react";
+
+type ValidationFunc<T> = (value: T) => boolean;
+
+export default function useStateWithValidation<T>(
+  validationFunc: ValidationFunc<T>,
+  initialValue: T,
+) {
+  const [state, setState] = useState<T>(initialValue);
+  const [isValid, setIsValid] = useState<boolean>(() => validationFunc(state));
+
+  const onChange = useCallback(
+    (nextState: T | ((prevState: T) => T)) => {
+      const value =
+        typeof nextState === "function"
+          ? (nextState as (prevState: T) => T)(state)
+          : nextState;
+      setState(value);
+      setIsValid(validationFunc(value));
+    },
+    [validationFunc, state],
+  );
+
+  return [state, onChange, isValid] as const;
+}
